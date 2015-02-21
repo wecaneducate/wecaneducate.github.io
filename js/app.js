a219c9c713ff977b0500bdd2cbaf2f900a9f2903
revanth <revanth@revanth-Inspiron-5520.(none)>
2015-02-21 15:23:08 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module("sitenameApp", ["ngRoute","sitenameController"]).config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'sitenameController1'
+      }).
+      when('/schools', {
+        templateUrl: 'partials/schools.html',
+        controller: 'sitenameController'
+      }).
+      when('/team', {
+        templateUrl: 'partials/team.html',
+        controller: 'sitenameController'
+      }).
+      when('/donate', {
+        templateUrl: 'partials/donate.html',
+        controller: 'sitenameController'
+      }).
+      when('/batches', {
+        templateUrl: 'partials/batches.html',
+        controller: 'sitenameController'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
+
